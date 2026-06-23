caa5d9a795b9c549c1ebefa466056ede8ab37666
ynandakishorereddy <yerrabandinandu00@gmail.com>
2026-06-23 11:58:37 +0530

    Allow null passwords for OAuth users

--- orcare_backend/migrations/001_create_tables.sql
+++ orcare_backend/migrations/001_create_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     age INTEGER,
     gender VARCHAR(50),
     district VARCHAR(255),
