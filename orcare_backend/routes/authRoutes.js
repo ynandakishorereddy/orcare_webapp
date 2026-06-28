b5c0cdccb56bf82b113add87968bfe4a5e2289c7
Yerrabandi Nandakishore Reddy <nandakishore35731@gmail.com>
2026-06-28 14:26:46 +0530

    Add some fooBar

--- orcare_backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { googleLogin, confirmDeleteAccount } = require('../controllers/authController_supabase');
-const { protect } = require('../middleware/authMiddleware_supabase');
-
-router.post('/google-login', googleLogin);
-router.post('/confirm-delete-account', protect, confirmDeleteAccount);
-
-module.exports = router;
--- orcare_backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProfile, updateProfile } = require('../controllers/userController_supabase');
-const { protect } = require('../middleware/authMiddleware_supabase');
-
-router.route('/profile').get(protect, getProfile).put(protect, updateProfile);
-router.route('/profile-update').post(protect, updateProfile);
-
-module.exports = router;
